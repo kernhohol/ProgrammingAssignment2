f7631b7a5908fd3465dc14fe9211c05a0b4f3282
Victor Ivanovsky <Victor Ivanovsky>
2015-11-22 17:39:46 +0300

    comments to both functions added

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
-## I check, if the cacheSolve function hd been ever run
-## If it run once, the las_cached matrix variable is set and can be compared to the one in makeCacheMatrix input
-## Also, in this case, I use previously used "m" value instead of settin it to NULL
+## I check, if the cacheSolve function had been ever run - that will be possible if a last_cached variable already exists
+## If it was run once, the last_cached matrix variable is set and can be compared to the one in makeCacheMatrix input
+## Also, in this case, I use previously cached (in cacheSolve function) "m" value instead of setting it to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -43,7 +43,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-##
+## If cacheSolve function run once, the last_cached matrix variable is set and can be used to check, what matrix goes to the makeCacheMatrix input
+## also, the m variable value is being stored in m2 variable to be used instead of NULL value in makeCachMatrix function
 
 cacheSolve <- function(x, ...) {
   
